7997db52e83b20968ad9a3ad194fbbafdf349319
Daniel-N-Huss <daniel.n.huss@gmail.com>
2020-05-06 20:00:36 +0000

    Initialized project, created a connection to server and refactored it to own file to allow further development of play components

---
+++ play.js
@@ -0,0 +1,9 @@
+// To connect to the server, use the following info:
+// IP Address: 50.64.116.162
+// Port: 50541
+
+const { connect } = require('./client');
+
+// Establish connection with the game server
+console.log('Connecting...');
+connect();
