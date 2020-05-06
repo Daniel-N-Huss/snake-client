9ea0755b3aec7a53ad64436add20a38d3a8ef6b3
Daniel-N-Huss <daniel.n.huss@gmail.com>
2020-05-06 20:37:03 +0000

    began adding in user input functionality. so far only able to exit program with ctrl-c

--- play.js
+++ play.js
@@ -8,10 +8,22 @@ const { connect } = require('./client');
 console.log('Connecting...');
 connect();
 
+const setupInput = function() {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding('utf8');
+  stdin.resume();
+  stdin.on('data', (handleUserInput) => {
+    if (handleUserInput === '\u0003') {
+      process.exit();
+    }
+  });
+  return stdin;
+};
 
+setupInput();
 
-
-/* 
+/*
 "Move: up" - move up one square (unless facing down)
 "Move: down" - move down one square (unless facing up)
 "Move: left" - move left one square (unless facing right)
